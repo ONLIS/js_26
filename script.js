77914d9afac5b64a51f321cdb4d7b50dbf5fff90
onlis <aleksei@onl.is>
2022-01-05 00:32:18 +0500

    lesson 26 har done

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 "use strict";
 
 const sendData = (user) => {
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    body: JSON.stringify(user),
-    headers: { "Content-type": "application/json; charset=UTF-8" },
-  });
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(user);
+  xhr.onload = function () {
+    if (xhr.status != 201) {
+      alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      alert(`Готово, отправлено ${xhr.response.length} байт`);
+    }
+  };
+
+  xhr.onprogress = function (event) {
+    if (event.lengthComputable) {
+      alert(`Отправлено ${event.loaded} из ${event.total} байт`);
+    } else {
+      alert(`Отправлено ${event.loaded} байт`);
+    }
+  };
+
+  xhr.onerror = function () {
+    alert("Запрос не удался");
+  };
 };
 
 const getData = () => {
   fetch("db.json")
     .then((response) => response.json())
     .then((data) => {
-      sendData(data);
+      sendData(JSON.stringify(data));
     })
     .catch((error) => console.log(error));
 };
